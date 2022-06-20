f3a4ed2e00a2f933d81f7a9c46d0e81cf13b779f
po-kun <webpp@po-kun.sakura.ne.jp>
2022-06-21 03:18:23 +0900

    update main

---
+++ src/App.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="container" id="box">
+        <div class="row min-vh-100">
+            <div class="col-3 border min-h-screen">
+                <div class="form-group">
+                    <label class="mt-5 mb-2">ページタイトル</label>
+                    <input class="form-control" type="text" name="left" v-on:focusIn="highlight" v-on:focusOut="highlightOut" v-model="message" />
+                </div>
+                <div class="form-group">
+                    <label class="mt-5 mb-2">ページの説明</label>
+                    <textarea class="form-control" type="textarea" name="left" v-model="description" />
+                </div>
+                <div class="form-group">
+                    <label class="mt-5 mb-2">動画並びを選択</label>
+                    <select class="form-select" v-model="selectedLayout" name="opts" aria-label="Default select example">
+                        <option value="3">4つ並び</option>
+                        <option value="4">3つ並び</option>
+                        <option value="6">2つ並び</option>
+                        <option value="12">1つ並び</option>
+                    </select>
+                </div>
+            </div>
+            <div class="col pt-5 px-5">
+                <h1>{{ message }}</h1>
+                <p v-html="brValue"></p>
+
+                <div class="row">
+                    <div v-bind:class="classOfImages">
+                        <span>title</span>
+                        <img class="img-fluid" src="https://placehold.jp/3d4070/ffffff/1920x1080.png?text=Video" alt="">
+                    </div>
+                    <div v-bind:class="classOfImages">
+                        <span>title</span>
+                        <img class="img-fluid" src="https://placehold.jp/3d4070/ffffff/1920x1080.png?text=Video" alt="">
+                    </div>
+                    <div v-bind:class="classOfImages">
+                        <span>title</span>
+                        <img class="img-fluid" src="https://placehold.jp/3d4070/ffffff/1920x1080.png?text=Video" alt="">
+                    </div>
+                    <div v-bind:class="classOfImages">
+                        <span>title</span>
+                        <img class="img-fluid" src="https://placehold.jp/3d4070/ffffff/1920x1080.png?text=Video" alt="">
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function () {
+        return {
+            message: '新年のご挨拶',
+            description: '2022年度 インタビュー動画一覧',
+            selectedLayout: '3'
+        }
+    },
+    computed : {
+        brValue : function(){
+            return this.description.replace(/\n/g, "<br />")
+        },
+        classOfImages : function(){
+            return `col-lg-${this.selectedLayout}`;
+        }
+    },
+    methods : {
+        highlight : function(){
+
+        },
+        highlightOut: function(){
+
+        },
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
